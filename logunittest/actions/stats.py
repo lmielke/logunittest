b6eec10d54d07f5862d4f7e9941f907ef15dd0ad
Lars Mielke <larsmielke2@gmail.com>
2022-12-30 11:47:21 +0100

    12-30 11:47 [all:2 ok:2 err:0]

--- logunittest/actions/stats.py
+++ logunittest/actions/stats.py
@@ -9,9 +9,10 @@ color.init()
 
 def main(*args, targetDir, **kwargs) -> None:
     found = Coverage()()
-    match = re.match('(<@>)(.*)(<@>)', found)
+    match, stats = re.match('(<@>)(.*)(<@>)', found), str()
     if match:
         for i, m in enumerate(match.group(2).split('!')):
+            stats += f'{m} '
             if i == 0:
                 print(f"{color.Fore.WHITE}{m}{color.Style.RESET_ALL}", end=' ')
             elif re.search(r'err:0', m):
@@ -20,8 +21,9 @@ def main(*args, targetDir, **kwargs) -> None:
                 print(f"{color.Fore.RED}{m}{color.Style.RESET_ALL}")
             else:
                 print(f"{color.Fore.YELLOW}{m}{color.Style.RESET_ALL}")
-
-
+    if not stats:
+        stats = 'no matching log found'
+    return stats.strip()
 
 
 if __name__ == "__main__":
--- logunittest/actions/ut.py
+++ logunittest/actions/ut.py
@@ -1,6 +1,7 @@
 # info.py
 import logunittest.settings as sts
 from logunittest.logunittest import ut as ut
+from logunittest.actions import stats as stats
 from subprocess import Popen
 import os, subprocess, sys
 import configparser
@@ -14,9 +15,11 @@ def main(*args, targetDir, comment:str=None, git_sync:bool=False, **kwargs) -> N
     ut(pgPath=targetDir)
     if git_sync:
         from logunittest.filestates import PipFileState
+        if comment is None: comment = stats.main(*args, targetDir=targetDir, **kwargs)
         with PipFileState(*args, targetDir=targetDir, **kwargs) as p:
             # print(f"Now pushing with modified Pipfile: {comment}")
             sts.gitSyncCmd.extend([f'"{targetDir}"', f'"{comment}"'])
+            print(f"{sts.gitSyncCmd = }")
             p = Popen(sts.gitSyncCmd, cwd=targetDir, stdout=sys.stdout)
             p.communicate()
 
