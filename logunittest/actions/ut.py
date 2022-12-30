e0b78b78bb4c07c82b5dd4c3fb66ebee35db7ac1
Lars Mielke <larsmielke2@gmail.com>
2022-12-30 11:57:39 +0100

    adding comment function [all:2 ok:2 err:0]

--- logunittest/actions/ut.py
+++ logunittest/actions/ut.py
@@ -11,18 +11,24 @@ import colorama as color
 color.init()
 
 
-def main(*args, targetDir, comment:str='', git_sync:bool=False, **kwargs) -> None:
-    comment = '' if comment is None else comment
+def main(*args, targetDir, git_sync:bool=False, **kwargs) -> None:
     ut(pgPath=targetDir)
     if git_sync:
         from logunittest.filestates import PipFileState
-        comment += f" [{stats.main(*args, targetDir=targetDir, **kwargs).split('[')[1]}"
+        comment = add_comment(*args, targetDir=targetDir, **kwargs)
         with PipFileState(*args, targetDir=targetDir, **kwargs) as p:
             # print(f"Now pushing with modified Pipfile: {comment}")
             sts.gitSyncCmd.extend([f'"{targetDir}"', f'"{comment}"'])
-            print(f"{sts.gitSyncCmd = }")
             p = Popen(sts.gitSyncCmd, cwd=targetDir, stdout=sys.stdout)
             p.communicate()
+
+def add_comment(*args, comment:str='', **kwargs) -> str:
+    """ 
+        adds a git comment in case testresults are committed and pushed to git
+    """
+    comment = 'lut push without comment' if comment is None else comment
+    comment += f" [{stats.main(*args, **kwargs).split('[')[1]}"
+    return comment
 
 if __name__ == "__main__":
     main()
