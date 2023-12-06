fdd4ce4b8dd27ac650d32a2c0362778d616baf08
Lars Mielke <larsmielke2@gmail.com>
2023-12-06 11:00:32 +0100

    auto push without message [all:12 ok:12 err:0]

--- logunittest/actions/ut.py
+++ logunittest/actions/ut.py
@@ -24,7 +24,9 @@ color.init()
 def main(*args, **kwargs) -> None:
     # assignes additional parameters to kwargs -> i.e. pgList, pythons
     ini0 = TestParams(*args, **kwargs)
-    run_uts(*args, **ini0.__dict__.get("pgList", os.getcwd()), **kwargs)
+    defaultPackage = {os.path.basename(os.getcwd()): os.getcwd()}
+    print(f"{defaultPackage = }")
+    run_uts(*args, **ini0.__dict__.get("pgList", defaultPackage), **kwargs)
     return kwargs["testId"]
 
 
